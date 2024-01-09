4ce875ec020151a3b74947a22c346214436f1627
37Ragavarshini <138793817+37Ragavarshini@users.noreply.github.com>
2024-01-09 12:50:13 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// hamburger menu
+
+hamburger = document.querySelector('.hamburger');
+hamburger.onclick = function(){
+    navBar = document.queryselector('.nav-bar');
+    navBar.classlist.toggle('active');
+
+}
